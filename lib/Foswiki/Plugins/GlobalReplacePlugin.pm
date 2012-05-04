a15e4855d044914172440880d2ba20470d6c525c
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/GlobalReplacePlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GlobalReplacePlugin.pm
+++ lib/Foswiki/Plugins/GlobalReplacePlugin.pm
@@ -22,7 +22,8 @@ use strict;
 
 our $VERSION = '$Rev$';
 our $RELEASE = $VERSION;
-our $SHORTDESCRIPTION = "Global search and replace functionality across all topics in a web";
+our $SHORTDESCRIPTION =
+  "Global search and replace functionality across all topics in a web";
 our $NO_PREFS_IN_TOPIC = 1;
 
 our $debug = 0;
@@ -30,7 +31,7 @@ our $debug = 0;
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
-    Foswiki::Func::registerTagHandler('GLOBALREPLACE', \&handleGlobalReplace);
+    Foswiki::Func::registerTagHandler( 'GLOBALREPLACE', \&handleGlobalReplace );
 
     return 1;
 }
@@ -40,86 +41,95 @@ sub handleGlobalReplace {
     my ( $session, $params, $topic, $web ) = @_;
 
     # parameter func can be: search, replace
-    my $func = $params->{func} ||
-        return "%RED%Invalid page access.%ENDCOLOR% Please go to " .
-               "GlobalSearchAndReplace to start your operation.";
-    my $param = $params->{param} || ""; # topic name
-    my $replaceSearchString = handleDecode($params->{rSearchString}) || "";
-    my $replaceString = handleDecode($params->{rString}) || "";
-    my $caseSensitive = $params->{caseSensitive} || "yes";
+    my $func = $params->{func}
+      || return "%RED%Invalid page access.%ENDCOLOR% Please go to "
+      . "GlobalSearchAndReplace to start your operation.";
+    my $param = $params->{param} || "";    # topic name
+    my $replaceSearchString = handleDecode( $params->{rSearchString} ) || "";
+    my $replaceString       = handleDecode( $params->{rString} )       || "";
+    my $caseSensitive       = $params->{caseSensitive}                 || "yes";
 
     #saved for Rob Hoffman's regular expression flag
     #$token = quotemeta( $token ) unless( $theRegex );
 
-    my $text = "";
-    my $aWeb = "";
-    my $aTopic = "";
+    my $text      = "";
+    my $aWeb      = "";
+    my $aTopic    = "";
     my $topicText = "";
 
     # Only certain people can commit a global search and replace
     # untaint $user to get rid of the Insecure Dependency
 
-    my $access = Foswiki::Func::isGroupMember($Foswiki::cfg{SuperAdminGroup});
+    my $access = Foswiki::Func::isGroupMember( $Foswiki::cfg{SuperAdminGroup} );
 
     if ( $func =~ /check/i ) {
         return "installed";
 
-    } elsif ( $func =~ /search/i && $param =~ /(.*)\.(.*)/) {
-        $aWeb = $1 || '';
+    }
+    elsif ( $func =~ /search/i && $param =~ /(.*)\.(.*)/ ) {
+        $aWeb   = $1 || '';
         $aTopic = $2 || '';
-        return "| [[$param][$aTopic]] | %RED% No =Replace Search String= " .
-               " entered. %ENDCOLOR% ||\n"
-            unless ( $replaceSearchString );
+        return "| [[$param][$aTopic]] | %RED% No =Replace Search String= "
+          . " entered. %ENDCOLOR% ||\n"
+          unless ($replaceSearchString);
 
         my $before = "";
-        my $after = "";
+        my $after  = "";
 
-        return "| [[$param][$aTopic]] | %RED% Topic does not exist %ENDCOLOR% ||\n"
-            unless ( Foswiki::Func::topicExists( $aWeb, $aTopic ) );
+        return
+          "| [[$param][$aTopic]] | %RED% Topic does not exist %ENDCOLOR% ||\n"
+          unless ( Foswiki::Func::topicExists( $aWeb, $aTopic ) );
         $topicText = Foswiki::Func::readTopicText( $aWeb, $aTopic );
 
         my $count = 0;
         my $hit;
         my $replace;
         my $position = 0;
-        my $lasPos = 0;
-        my $first = "";
-        my $second = "";
-        my $third = "";
-        my $fourth = "";
+        my $lasPos   = 0;
+        my $first    = "";
+        my $second   = "";
+        my $third    = "";
+        my $fourth   = "";
+
         # save a copy so that the capture can be reset for each match
         my $orgReplaceString = $replaceString;
         my $lastPos;
 
         while (1) {
+
             # reseting variables that allow the user to capture
-            $first = "";
-            $second = "";
-            $third = "";
-            $fourth = "";
+            $first         = "";
+            $second        = "";
+            $third         = "";
+            $fourth        = "";
             $replaceString = $orgReplaceString;
 
             # Make sure to grab a little before and after the hit
             # Try to grab the whole word instead of breaking it.
 
             if ( $caseSensitive =~ /yes/i ) {
-                last unless $topicText =~ m/((?:^|\s|[a-zA-Z0-9\.]*).{0,40}?)($replaceSearchString)(.{0,40}[a-zA-Z0-9\.]*)/gs;
+                last
+                  unless $topicText =~
+m/((?:^|\s|[a-zA-Z0-9\.]*).{0,40}?)($replaceSearchString)(.{0,40}[a-zA-Z0-9\.]*)/gs;
                 $before = $1;
-                $hit = $2;
-                $first = $3 || "";
+                $hit    = $2;
+                $first  = $3 || "";
                 $second = $4 || "";
-                $third = $5 || "";
+                $third  = $5 || "";
                 $fourth = $6 || "";
-                $after = $+;
-            } else {
-                last unless $topicText =~ m/((?:^|\s|[a-zA-Z0-9\.]*).{0,40}?)($replaceSearchString)(.{0,40}[a-zA-Z0-9\.]*)/gis;
+                $after  = $+;
+            }
+            else {
+                last
+                  unless $topicText =~
+m/((?:^|\s|[a-zA-Z0-9\.]*).{0,40}?)($replaceSearchString)(.{0,40}[a-zA-Z0-9\.]*)/gis;
                 $before = $1;
-                $hit = $2;
-                $first = $3 || "";
+                $hit    = $2;
+                $first  = $3 || "";
                 $second = $4 || "";
-                $third = $5 || "";
+                $third  = $5 || "";
                 $fourth = $6 || "";
-                $after = $+;
+                $after  = $+;
             }
 
             $replaceString =~ s/\$1/$first/gos;
@@ -128,86 +138,93 @@ sub handleGlobalReplace {
             $replaceString =~ s/\$4/$fourth/gos;
             $replaceString =~ s/\$topic/$topic/gos;
 
-            # reposition cursor in case there is a hit in the after
-            # In resulting hits in after, will not have much in the leading before text.
+    # reposition cursor in case there is a hit in the after
+    # In resulting hits in after, will not have much in the leading before text.
             $lastPos = $position;
-            $position = (pos $topicText) - (length $after);
+            $position = ( pos $topicText ) - ( length $after );
 
             last if ( $lastPos == $position );
 
             pos $topicText = $position;
 
-            # Encode with %(H|R)COLOR% Tags to highlight the hits and replace text
-            $hit = "$before%HCOLOR%$hit%ENDCOLOR%$after";
-            $hit = escapeSpecialChars($hit);
+          # Encode with %(H|R)COLOR% Tags to highlight the hits and replace text
+            $hit     = "$before%HCOLOR%$hit%ENDCOLOR%$after";
+            $hit     = escapeSpecialChars($hit);
             $replace = "$before%RCOLOR%$replaceString%ENDCOLOR%$after";
             $replace = escapeSpecialChars($replace);
 
-            $text .= "|  <input type=\"checkbox\" name=\"SEARCH_"
-                  . "$aWeb" . "_" . $aTopic . "_" . ++$count . "\">";
-            $text .= "  | <tt>$hit</tt> "
-                   . " | <tt>$replace</tt> |\n";
+            $text .=
+                "|  <input type=\"checkbox\" name=\"SEARCH_" . "$aWeb" . "_"
+              . $aTopic . "_"
+              . ++$count . "\">";
+            $text .= "  | <tt>$hit</tt> " . " | <tt>$replace</tt> |\n";
 
         }
 
         my $temp = "";
-        $temp = "<font size=\"-1\">- $count hits</font>" if( $count > 1 );
+        $temp = "<font size=\"-1\">- $count hits</font>" if ( $count > 1 );
 
-        my( $lock, $tmp1, $tmp2 ) = &Foswiki::Func::checkTopicEditLock( $aWeb, $aTopic );
-        $tmp1 = ""; $tmp2 = ""; # suppress warnings
-        if( $lock ) {
+        my ( $lock, $tmp1, $tmp2 ) =
+          &Foswiki::Func::checkTopicEditLock( $aWeb, $aTopic );
+        $tmp1 = "";
+        $tmp2 = "";    # suppress warnings
+        if ($lock) {
             $lock = "%RED%(LOCKED)%ENDCOLOR%";
-        } else {
+        }
+        else {
             $lock = "";
         }
 
-        $text = "| [[$param][$aTopic]] $temp $lock |||\n" . $text if ( $text );
+        $text = "| [[$param][$aTopic]] $temp $lock |||\n" . $text if ($text);
 
-    } elsif ( $func =~ /replace/i ) {
+    }
+    elsif ( $func =~ /replace/i ) {
 
-        return "You are currently logged in as " .
-          Foswiki::Func::userToWikiName($session->{user})
-               . ". %RED%Only Members of the Main." . $Foswiki::cfg{SuperAdminGroup} . " may save the changes "
-               . "of a Global Search And Replace. %ENDCOLOR%"
-            unless ($access);
+        return
+            "You are currently logged in as "
+          . Foswiki::Func::userToWikiName( $session->{user} )
+          . ". %RED%Only Members of the Main."
+          . $Foswiki::cfg{SuperAdminGroup}
+          . " may save the changes "
+          . "of a Global Search And Replace. %ENDCOLOR%"
+          unless ($access);
 
-        return "%RED% No =Replace Search String= " .
-               " entered %ENDCOLOR% Back to GlobalSearchAndReplace.\n"
-            unless ( $replaceSearchString );
+        return "%RED% No =Replace Search String= "
+          . " entered %ENDCOLOR% Back to GlobalSearchAndReplace.\n"
+          unless ($replaceSearchString);
 
         # reactivation tabs, carriage returns and newlines
         # for some reason, somewhere in the processing it became a string rather
         # what the \t, \n, \r should represent
-        $replaceString =~ s/\\t/chr(9)/eg; # tab
-        $replaceString =~ s/\\n/chr(10)/eg; # new line
-        $replaceString =~ s/\\r/chr(13)/eg; # carriage return
+        $replaceString =~ s/\\t/chr(9)/eg;     # tab
+        $replaceString =~ s/\\n/chr(10)/eg;    # new line
+        $replaceString =~ s/\\r/chr(13)/eg;    # carriage return
 
         # getting checkbox parameters
         my $cgi = &Foswiki::Func::getCgiQuery();
-        if (! $cgi ) {
+        if ( !$cgi ) {
             return "";
         }
 
         # parsing checkbox parameters
         my @topicList = map { s/^SEARCH_//o; $_ }
-                        grep { /^SEARCH/ }
-                        $cgi->param;
+          grep { /^SEARCH/ } $cgi->param;
 
         # maintaining a list of unique topic names
-        my %topicHash = map { m/(.*?)_(.*)_.*/; "$1.$2" => 1} @topicList;
+        my %topicHash = map { m/(.*?)_(.*)_.*/; "$1.$2" => 1 } @topicList;
 
-        my $count = 0; # counter for the number of replacements possible
-        my $replaced = 0; # counter for the number of actual replacements done
+        my $count    = 0;   # counter for the number of replacements possible
+        my $replaced = 0;   # counter for the number of actual replacements done
         my $displayTopicText = "";
         foreach my $key ( sort keys %topicHash ) {
             $key =~ /(.*)\.(.*)/;
-            $aWeb = $1;
+            $aWeb   = $1;
             $aTopic = $2;
 
             $topicText = &Foswiki::Func::readTopicText( $aWeb, $aTopic );
 
             # reset counters
-            $count = 1;
+            $count    = 1;
             $replaced = 0;
             if ( $caseSensitive =~ /yes/i ) {
                 $topicText =~ s/($replaceSearchString)/
@@ -216,27 +233,31 @@ sub handleGlobalReplace {
                              \$count, \@topicList,
                              $aTopic, \$replaced )/geos;
 
-            } else {
+            }
+            else {
                 $topicText =~ s/($replaceSearchString)/
                   doReplace( $1, $2, $3, $4, $5,
                              $replaceString,
                              \$count, \@topicList,
                              $aTopic, \$replaced )/igeos;
             }
-            $displayTopicText = escapeSpecialChars( $topicText );
+            $displayTopicText = escapeSpecialChars($topicText);
             $text .= "| [[$key][$aTopic]] | $replaced";
-            $text .= "<br />DEBUG MESSAGE:<br /><tt>$displayTopicText</tt>" if $debug;
+            $text .= "<br />DEBUG MESSAGE:<br /><tt>$displayTopicText</tt>"
+              if $debug;
             $text .= " |\n";
 
             # Save changes to text here
-            &Foswiki::Func::writeDebug( "GlobalReplacePlugin::saving") if ( $debug );
+            &Foswiki::Func::writeDebug("GlobalReplacePlugin::saving")
+              if ($debug);
             Foswiki::Func::saveTopicText( $aWeb, $aTopic, $topicText );
         }
-        if ( $text ) {
+        if ($text) {
             $text = "| *Topic* | *Number of Replacements* |\n" . $text;
-        } else {
+        }
+        else {
             $text = "No Replacements done. Back to GlobalSearchAndReplace, "
-                  . "GlobalReplacePlugin";
+              . "GlobalReplacePlugin";
         }
     }
 
@@ -245,14 +266,20 @@ sub handleGlobalReplace {
 
 # =========================
 sub doReplace {
-    my ( $hit, $first, $second, $third, $fourth, $replace, $count, $topicList, $topic, $replaced ) = @_;
+    my (
+        $hit,     $first, $second,    $third, $fourth,
+        $replace, $count, $topicList, $topic, $replaced
+    ) = @_;
 
-    my ( $flag ) = grep { /.*\_$topic\_$$count/ } @$topicList;
-    &Foswiki::Func::writeDebug( "- GlobalReplacePlugin::doReplace($hit, $replace, "
-                              . $$count . ", $topic, $flag, )" ) if $debug;
-    $first = "" unless ($first);
+    my ($flag) = grep { /.*\_$topic\_$$count/ } @$topicList;
+    &Foswiki::Func::writeDebug(
+            "- GlobalReplacePlugin::doReplace($hit, $replace, " 
+          . $$count
+          . ", $topic, $flag, )" )
+      if $debug;
+    $first  = "" unless ($first);
     $second = "" unless ($second);
-    $third = "" unless ($third);
+    $third  = "" unless ($third);
     $fourth = "" unless ($fourth);
 
     $replace =~ s/\$1/$first/gos;
@@ -262,10 +289,11 @@ sub doReplace {
     $replace =~ s/\$topic/$topic/gos;
 
     ++$$count;
-    if ( $flag ) {
+    if ($flag) {
         ++$$replaced;
         return $replace;
-    } else {
+    }
+    else {
         return $hit;
     }
 
@@ -276,13 +304,14 @@ sub doReplace {
 
 # =========================
 sub escapeSpecialChars {
-    my ( $string ) = @_;
+    my ($string) = @_;
 
     $string =~ s/\&/&amp;/go;
     $string =~ s/\</&lt;/go;
     $string =~ s/\>/&gt;/go;
     $string =~ s/\=/&#61;/go;
     $string =~ s/\_/&#95;/go;
+
     # Highlight the hit and replace strings w/ color
     $string =~ s/\%HCOLOR\%/<font color=\"#FF0000\"><b>/go;
     $string =~ s/\%RCOLOR\%/<font color=\"#FF6600\"><b>/go;
@@ -294,7 +323,7 @@ sub escapeSpecialChars {
     $string =~ s/\[/&#91;/go;
     $string =~ s/\]/&#93;/go;
     $string =~ s/\|/&#124;/go;
-    $string =~ s/([\s\(])([A-Z])/$1<nop>$2/go; # defuse WikiWord links
+    $string =~ s/([\s\(])([A-Z])/$1<nop>$2/go;    # defuse WikiWord links
     $string =~ s/\t/        /go;
     $string =~ s/  /&nbsp; /go;
     $string =~ s/[\n\r]+/<br \/>/gos;
@@ -304,7 +333,7 @@ sub escapeSpecialChars {
 
 # =========================
 sub handleDecode {
-    my( $theStr ) = @_;
+    my ($theStr) = @_;
     $theStr ||= '';
 
     # entity decode - Cairo: &#34;, Dakar: &#034;
